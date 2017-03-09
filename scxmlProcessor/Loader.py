d778ce271a1312404fe9297ca0441d38e326ce66
Cesar Colle <cesar.colle@etu.unice.fr>
2017-03-09 19:09:29 +0100

    creating first template

--- scxmlProcessor/Loader.py
+++ scxmlProcessor/Loader.py
@@ -1,19 +1,42 @@
+from __future__ import print_function
+
 import sets
 
 from scxml.pyscxml import StateMachine
+from scxml.interpreter import Transition
+import re
 from scxml.node import SCXMLNode
+import json
 
 ## The loader class allow us to load a SCXML file.
 ## The loader will provide the tree containing all the data needed by our generator.
 
 informations = dict()
 
-class Loader :
+
+class Loader:
     def __init__(self, path):
         self.machine = StateMachine(path)
-        self.data = self.machine.doc.stateDict["Initial"]
+        f = lambda key, g: print(key, g)
+        self.data = reduce(f , self.machine.doc.stateDict.keys())
+        self.data = dict()
+
+
+    def generateTransition(self):
+        self.t = Transition(self.machine.doc.rootState)
+        for key in self.machine.doc.stateDict:
+            tmp = Transition(self.machine.doc.stateDict[key])
+            for transition in tmp:
+                self.data[key] = self.data[key] + list("".join(re.search("(?<==).*?")))
+
+
     def __str__(self):
         l = ""
         for s in self.machine.doc.stateDict["Initial"].transition:
-            informations[s] = {"parent" : s.parent}
+            informations[s] = {"parent": s.parent}
         return l
+
+
+if __name__ == "__main__":
+    l = Loader("../test.xml")
+    l.generateTransition()
--- templateGenerator/Generator.py
+++ templateGenerator/Generator.py
@@ -1,6 +1,6 @@
 ####  Generator class will generate via Cheetah template some files .
 
-from statechart_debugout import statechart_debugout
+from state_generic_template import state_generic_template
 from scxmlProcessor.Loader import Loader
 
 
@@ -8,7 +8,7 @@ class Generator:
 
 
     def generateChart(self, stateChartXML, fileName, template):
-        template.stateChartXML = stateChartXML
+        template.state = stateChartXML
         self.generateOutputFile(fileName, template)
 
     def generateOutputFile(self, fileName, template):
@@ -22,8 +22,9 @@ class Generator:
 
 if __name__ == '__main__':
     loader = Loader("../test.xml")
-    template = statechart_debugout()
+    template = state_generic_template()
     generator = Generator()
-    generator.generateChart(loader.data,
+
+    generator.generateChart(loader.test,
                             './test.out',
                             template)
